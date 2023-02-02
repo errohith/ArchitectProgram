db2fbaf0f79f2884cc9d708bb58346adbf731dd5
errohith <errohith@gmail.com>
2023-02-02 21:41:48 +0530

    Updated

--- src/main/java/patternwithretry/BridgeDesignPatternRetryTest.java
+++ src/main/java/patternwithretry/BridgeDesignPatternRetryTest.java
@@ -21,10 +21,10 @@ public class BridgeDesignPatternRetryTest {
 		engine.click("//a[contains(text(),'CRM/SFA')]");
 		engine.click("//a[text()='Leads']");
 		engine.click("//a[text()='Create Lead']");
-		engine.type("//input[@id='createLeadForm_companyName']", "Team A");
-		engine.type("//input[@id='createLeadForm_firstName']", "A");
-		engine.type("//input[@id='createLeadForm_lastName']", "Team");
-		engine.type("//input[@id='createLeadForm_primaryEmail']", "team_a@testleaf.com");
+		engine.type("//input[@id='createLeadForm_companyName']", "Group");
+		engine.type("//input[@id='createLeadForm_firstName']", "6");
+		engine.type("//input[@id='createLeadForm_lastName']", "Group6");
+		engine.type("//input[@id='createLeadForm_primaryEmail']", "Group@testleaf.com");
 		engine.click("//input[@name='submitButton']");
 		System.out.println(engine.getText("//span[@id='viewLead_companyName_sp']"));
 		
